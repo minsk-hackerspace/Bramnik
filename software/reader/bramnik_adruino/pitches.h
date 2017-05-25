0e2e5c899434418aa967050b976b4355c68b45c3
Flop <flop@apalon.com>
2017-05-25 22:57:33 +0300

    tune some music for feedback

--- software/reader/bramnik_adruino/pitches.h
+++ software/reader/bramnik_adruino/pitches.h
@@ -93,22 +93,3 @@
 #define NOTE_DS8 4978
 
 
-const int c = 261;
-const int d = 294;
-const int e = 329;
-const int f = 349;
-const int g = 391;
-const int gS = 415;
-const int a = 440;
-const int aS = 455;
-const int b = 466;
-const int cH = 523;
-const int cSH = 554;
-const int dH = 587;
-const int dSH = 622;
-const int eH = 659;
-const int fH = 698;
-const int fSH = 740;
-const int gH = 784;
-const int gSH = 830;
-const int aH = 880;
