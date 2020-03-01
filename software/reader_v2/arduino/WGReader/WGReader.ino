0902a97cdbfa81679c8ad8b75cd95848cb87ecec
Flop <flop@hackerspace.by>
2020-03-01 17:21:40 +0300

    Update reader soft

---
+++ software/reader_v2/arduino/WGReader/WGReader.ino
@@ -0,0 +1,25 @@
+#include "Wiegand.h"
+
+WIEGAND wg;
+
+void setup() {
+	Serial.begin(9600);  
+	
+	// default Wiegand Pin 2 and Pin 3 see image on README.md
+	// for non UNO board, use wg.begin(pinD0, pinD1) where pinD0 and pinD1 
+	// are the pins connected to D0 and D1 of wiegand reader respectively.
+	wg.begin();
+}
+
+void loop() {
+	if(wg.available())
+	{
+		Serial.print("Wiegand HEX = ");
+		Serial.print(wg.getCode(),HEX);
+		Serial.print(", DECIMAL = ");
+		Serial.print(wg.getCode());
+		Serial.print(", Type W");
+		Serial.println(wg.getWiegandType());    
+	}
+ delay(3000);
+}
--- software/reader_v2/arduino/reader/reader.ino
+++ software/reader_v2/arduino/reader/reader.ino
@@ -2,7 +2,7 @@
  * Example on how to use the Wiegand reader library with interruptions.
  */
 
-#include <Wiegand.h>
+#include "Wiegand.h" //https://github.com/paulo-raca/YetAnotherArduinoWiegandLibrary
 #include <Wire.h>
 
 // These are the pins connected to the Wiegand D0 and D1 signals.
@@ -11,19 +11,27 @@
 #define PIN_D1 3
 
 #define PIN_INT 10
-#define PIN_GREENLED 5
-#define PIN_BEEP 4
+#define PIN_GREENLED 4
+#define PIN_BEEP 5
+#define PIN_LOCK 9
+#define PIN_ONBOARDLED 13
 
-#define CMD_BEEP      (1<<0)
-#define CMD_GREENLED  (1<<1)
+#define CMD_BEEP       (1<<0)
+#define CMD_GREENLED   (1<<1)
+#define CMD_OPEN       (1<<2)
+#define CMD_DENY       (1<<3)
+#define CMD_READER_ON  (1<<4)
+#define CMD_READER_OFF (1<<5)
 
 // The object that handles the wiegand protocol
 Wiegand wiegand;
 
 enum reader_event_type {
-  EVENT_ID = 0,
+  EVENT_CARD = 0,
   EVENT_ERROR = 1,
   EVENT_STATE = 2,
+  EVENT_CODE = 3,
+  EVENT_DOOR = 4,
   EVENT_NODATA = 0xfe,
 };
 struct reader_event {
@@ -39,6 +47,78 @@ static uint8_t buf_tail = 0;
 
 #define EVENTS_COUNT() ((buf_head - buf_tail + BUFFER_SIZE) % BUFFER_SIZE)
 
+bool readerEnabled = false;
+
+uint8_t keypadData[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t keypadData_HARDCODED_PASS[4] = {0x00, 0x00, 0x00, 0x00};
+
+bool keypadData_HARDCODED_PASS_CORRECT = false;
+
+uint8_t keypadDataIdx = 0;
+uint8_t keypadDataTiks = 0;
+#define MAX_KEY_WAIT_TICKS 20
+
+void keypadAddKey(uint8_t key) {
+  
+  Serial.print(" > key : ");
+  Serial.print(key);
+  Serial.println();
+  if (key == 10) {
+    keypadClear();
+  } else if (key == 11) {
+    keypadFlush();
+  } else if (keypadDataIdx < 8) {
+
+    uint8_t k = keypadData[keypadDataIdx/2];
+
+    if (keypadDataIdx % 2 == 0) {
+      k = ( k & 0x0f ) | ((key & 0x0F) << 4);
+    } else {
+      k = ( k & 0xf0 ) | (key & 0x0F);
+    }
+    keypadData[keypadDataIdx/2] = k;
+    keypadDataIdx ++;
+    keypadDataTiks = 0;
+    
+  } else if (keypadDataIdx > 32) {
+    keypadClear();
+  }
+
+}
+
+void keypadClear() {
+  for (int i=0; i<4; i++) {
+    keypadData[i] = 0xFF;
+  }
+  keypadDataIdx = 0;
+  Serial.println(" > key clear ");
+}
+
+void keypadFlush() {
+  Serial.print(" > key buff : ");
+  for (int i=0; i<4; i++) {
+    Serial.print(keypadData[i] >> 4, 16);
+    Serial.print(keypadData[i] & 0xF, 16);
+  }
+  Serial.println();
+
+  if (memcmp(keypadData, keypadData_HARDCODED_PASS, 4) == 0) {
+    keypadData_HARDCODED_PASS_CORRECT = true;
+  }
+
+  struct reader_event ev;
+  ev.event = EVENT_CODE;
+  ev.param = 4;
+  ev.data[0] = keypadData[0];
+  ev.data[1] = keypadData[1];
+  ev.data[2] = keypadData[2];
+  ev.data[3] = keypadData[3];  
+  event_push(&ev);    
+
+  keypadClear();
+  
+}
+
 bool event_push(struct reader_event *reader_event)
 {
   if (EVENTS_COUNT() == (BUFFER_SIZE - 1))
@@ -48,7 +128,8 @@ bool event_push(struct reader_event *reader_event)
   
   buf_head++;
   buf_head %= BUFFER_SIZE;
-    
+
+  digitalWrite(PIN_INT, 0);
   return true;
 }
 
@@ -64,21 +145,75 @@ bool event_pop(struct reader_event *reader_event)
     return true;
 }
 
+
+void unlock() {
+  Serial.println("LOCK OPEN");
+  digitalWrite(PIN_LOCK, HIGH);
+  digitalWrite(PIN_ONBOARDLED, HIGH);
+
+  for (int i=0; i<4; i++) {
+    digitalWrite(PIN_GREENLED, LOW);
+    digitalWrite(PIN_BEEP, LOW);
+    delay(100);
+    digitalWrite(PIN_GREENLED, HIGH);
+    digitalWrite(PIN_BEEP, HIGH);
+    delay(100);
+  }
+
+  digitalWrite(PIN_LOCK, LOW);
+  digitalWrite(PIN_ONBOARDLED, LOW);
+}
+
+void accessDeny() {
+  digitalWrite(PIN_BEEP, LOW);
+  delay(1000);
+  digitalWrite(PIN_BEEP, HIGH);
+}
+
+void accessGrant() {
+  unlock();
+}
+
+
+void enableReader(bool enable) {
+
+  if (readerEnabled == true && enable == false) {
+    //disable
+    wiegand.flushNow();
+    wiegand.end();
+    detachInterrupt(digitalPinToInterrupt(PIN_D0));
+    detachInterrupt(digitalPinToInterrupt(PIN_D1));
+    readerEnabled = false;
+    Serial.println("READER DISABLED");
+  }
+  
+  if (readerEnabled == false && enable == true) {
+    //enable
+
+    //Install listeners and initialize Wiegand reader
+    wiegand.onReceive(receivedData, "Card readed: ");
+    wiegand.onReceiveError(receivedDataError, "Card read error: ");
+    wiegand.onStateChange(stateChanged, "State changed: ");
+    wiegand.begin(Wiegand::LENGTH_ANY, true);
+  
+    //initialize pins as INPUT and attaches interruptions
+    pinMode(PIN_D0, INPUT);
+    pinMode(PIN_D1, INPUT);
+    attachInterrupt(digitalPinToInterrupt(PIN_D0), pinStateChanged, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_D1), pinStateChanged, CHANGE);
+
+    readerEnabled = true;
+    //Sends the initial pin state to the Wiegand library
+    pinStateChanged();
+    Serial.println("READER ENABLED");
+  }
+  
+}
+
 // Initialize Wiegand reader
 void setup() {
   Serial.begin(9600);
 
-  //Install listeners and initialize Wiegand reader
-  wiegand.onReceive(receivedData, "Card readed: ");
-  wiegand.onReceiveError(receivedDataError, "Card read error: ");
-  wiegand.onStateChange(stateChanged, "State changed: ");
-  wiegand.begin(Wiegand::LENGTH_ANY, true);
-
-  //initialize pins as INPUT and attaches interruptions
-  pinMode(PIN_D0, INPUT);
-  pinMode(PIN_D1, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PIN_D0), pinStateChanged, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PIN_D1), pinStateChanged, CHANGE);
 
   Wire.begin(0x06);
   Wire.onRequest(i2c_request_cb);
@@ -92,9 +227,26 @@ void setup() {
   
   digitalWrite(PIN_GREENLED, HIGH);
   pinMode(PIN_GREENLED, OUTPUT);
+
+  digitalWrite(PIN_LOCK, LOW);
+  pinMode(PIN_LOCK, OUTPUT);
+
+  digitalWrite(PIN_ONBOARDLED, LOW);
+  pinMode(PIN_ONBOARDLED, OUTPUT);
   
-  //Sends the initial pin state to the Wiegand library
-  pinStateChanged();
+  
+//  TODO COMMENT
+//  delay(200);
+//  accessDeny();
+//  delay(200);
+//  accessGrant();
+
+  delay(100);
+  
+  //Initialize Wiegand reader and
+
+  enableReader(true);
+  
 }
 
 // Every few milliseconds, check for pending messages on the wiegand reader
@@ -104,6 +256,23 @@ void loop() {
   wiegand.flush();
   interrupts();
   //Sleep a little -- this doesn't have to run very often.
+
+  if (keypadData_HARDCODED_PASS_CORRECT) {
+    keypadData_HARDCODED_PASS_CORRECT = false;
+    delay(100);
+    Serial.print(" > key HARDCODED YAY !!! ");
+    delay(100);
+    accessGrant();
+    delay(200);
+  }
+
+  if (keypadDataIdx != 0 && keypadDataTiks > MAX_KEY_WAIT_TICKS) {
+    keypadClear();
+  } else {
+    keypadDataTiks ++;
+  }
+  
+
   delay(100);
 }
 
@@ -159,28 +328,35 @@ void stateChanged(bool plugged, const char* message) {
 // Notifies when a card was read.
 // Instead of a message, the seconds parameter can be anything you want -- Whatever you specify on `wiegand.onReceive()`
 void receivedData(uint8_t* data, uint8_t bits, const char* message) {
-    struct reader_event ev;
 
-    ev.event = EVENT_ID;
-    ev.param = bits;
-    uint8_t bytes = (bits+7)/8;
-    for (int i = 0; i < bytes; i++) {
-      ev.data[i] = data[i];
-    }
-
-    event_push(&ev);
-    digitalWrite(PIN_INT, 0);
-    
     Serial.print(message);
     Serial.print(bits);
-    Serial.print("bits / ");
+    Serial.print(" bits / data: ");
     //Print value in HEX
-    
+    uint8_t bytes = (bits+7)/8;
     for (int i=0; i<bytes; i++) {
         Serial.print(data[i] >> 4, 16);
         Serial.print(data[i] & 0xF, 16);
     }
     Serial.println();
+
+    if (bits == 4) {
+      //keypad code enter
+      keypadAddKey(data[0]);
+      return;
+    }
+
+    struct reader_event ev;
+
+    ev.event = EVENT_CARD;
+    ev.param = bits;
+    
+    for (int i = 0; i < bytes; i++) {
+      ev.data[i] = data[i];
+    }
+
+    event_push(&ev);    
+    
 }
 
 // Notifies when an invalid transmission is detected
